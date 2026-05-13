e92118344280d94fb8e7596da92532d37c61a5a1
acrisrosa <cris2022pereira@gmail.com>
2026-05-13 11:43:13 +0100

    exercicio com as tags em html

--- aula3_tarefa3/src/app/tag-listas/tag-listas.ts
+++ aula3_tarefa3/src/app/tag-listas/tag-listas.ts
@@ -1,3 +1,4 @@
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './tag-listas.html',
   styleUrl: './tag-listas.css',
 })
-export class TagListas {}
+export class TagListas {
+
+  /**Isto é uma string */
+  minhaString: string = "Paulo";
+ 
+  /**Isto é um ARRAY de String, ou um predio de string na memória */
+  minhaLista: string [] = ["Tony", "DATE_PIPE_DEFAULT_OPTIONS", "samara", "leonardo", "Priscila", "Sara", "1", "false"];
+  
+  /**Minha Lista de Any, não é comum usar o any, e ele desliga o TypeScript */
+  minhaListaJS: any [] = ["Tony", "DATE_PIPE_DEFAULT_OPTIONS", "samara", "leonardo", "Priscila", "Sara", 1, false];
+ 
+  /**Minha Lista (Super Array) com error */
+  //minhaListaJSNumber: number [] = ["Tony", "DATE_PIPE_DEFAULT_OPTIONS", "samara", "leonardo", "Priscila", "Sara", "1", "false"];
+}
