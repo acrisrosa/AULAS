2d6677980e6f450fda13fca4822b65e13b0ea1f1
acrisrosa <cris2022pereira@gmail.com>
2026-05-14 08:31:47 +0100

    TAG IMAGEM, VIDEO E SEMANTICA

--- aula4_tpc1/src/app/app.ts
+++ aula4_tpc1/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TagsImg } from "./tags-img/tags-img";
+import { TagsVideo } from "./tags-video/tags-video";
+import { TagsSemanticas } from "./tags-semanticas/tags-semanticas";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TagsImg, TagsVideo, TagsSemanticas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
