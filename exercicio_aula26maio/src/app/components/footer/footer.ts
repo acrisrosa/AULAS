5e1fb7f2831a17c1f7814d160e5786893207b1a3
acrisrosa <cris2022pereira@gmail.com>
2026-05-28 09:59:51 +0100

    terminado o exercicio

--- exercicio_aula26maio/src/app/components/footer/footer.ts
+++ exercicio_aula26maio/src/app/components/footer/footer.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  localData: string;
+
+  constructor () {
+    const agora = DateTime.now();
+    this.localData = agora.toLocaleString(DateTime.DATETIME_FULL);
+
+  }
+}
