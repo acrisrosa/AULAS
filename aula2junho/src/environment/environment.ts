059933c1c6dec3f393f126b6469aed86ff63a238
acrisrosa <cris2022pereira@gmail.com>
2026-06-02 21:44:17 +0100

    continuação aula

---
+++ aula2junho/src/environment/environment.ts
@@ -0,0 +1,13 @@
+import { keys } from './environment.keys';
+
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: keys.apiKey,
+        authDomain: 'frontend-cinel-cris.firebaseapp.com',
+        projectId: 'frontend-cinel-cris',
+        storageBucket: 'frontend-cinel-cris.firebasestorage.app',
+        messagingSenderId: '983963187561',
+        appId: '1:983963187561:web:8ec88ecac494b32cc865f8'
+},
+}
