13474d8424ed06675ae31088e7bf24e8d2c5962c
acrisrosa <cris2022pereira@gmail.com>
2026-06-07 18:43:31 +0100

    environment do projeto

---
+++ teste7junho/src/environment/environment.prod.ts
@@ -0,0 +1,18 @@
+import { keys } from "./environment.keys"
+
+export const environment={
+    prodution: true,
+    firebaseConfig: {
+        apiKey: keys.apiKey,
+        authDomain: "frontend-cinel-cris-teste7.firebaseapp.com",
+        projectId: "frontend-cinel-cris-teste7",
+        storageBucket: "frontend-cinel-cris-teste7.firebasestorage.app",
+        messagingSenderId: "730624804558",
+        appId: "1:730624804558:web:94d84e0a624a4527ebb29f"
+    },
+};
+
+
+
+
+
---
+++ teste7junho/src/environment/environment.ts
@@ -0,0 +1,14 @@
+import { keys } from "./environment.keys"
+
+export const environment={
+    prodution: false,
+    firebaseConfig: {
+        apiKey: keys.apiKey,
+        authDomain: "frontend-cinel-cris-teste7.firebaseapp.com",
+        projectId: "frontend-cinel-cris-teste7",
+        storageBucket: "frontend-cinel-cris-teste7.firebasestorage.app",
+        messagingSenderId: "730624804558",
+        appId: "1:730624804558:web:94d84e0a624a4527ebb29f"
+    },
+};
+
