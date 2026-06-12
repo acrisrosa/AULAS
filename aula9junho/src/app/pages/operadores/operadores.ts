b5b07ac83d7bc3def849cc48e7c6538608687fd0
acrisrosa <cris2022pereira@gmail.com>
2026-06-12 06:53:51 +0100

    operadores e variaveis

--- aula9junho/src/app/pages/operadores/operadores.ts
+++ aula9junho/src/app/pages/operadores/operadores.ts
@@ -1,9 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-operadores',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './operadores.html',
   styleUrl: './operadores.css',
 })
-export class Operadores {}
+export class Operadores {
+  ///Operador aritimetricos
+  //soma, subtração, multiplicação, divisão e resto
+  localAritimeticos: string = '+, -, *, /, %';
+
+  //Operador de comparação
+  //comparação sem tipagem, comparação com tipagem, diferente sem tipagem, diferente com tipagem, maior, menor, maior&igual, menor&igual
+  localComparacao: string = '==, ===, !=, !==, >, <, >=, <=';
+
+  exemploOperador = '3'; // se usar == o compilador vai falar que é if (3 == exemploOperador); true
+  exemploOperador3iguais = '3'; // se usar === o compilador vai falar que é if (3 === exemploOperador); false
+
+  //Operadores Lógicos
+  //&&=e é 100% true, ||=ou pode ter 1 true, ?? coalescência nula em javascript foi usada na imagem do avatar, (substitui o if e else)
+  localLogicos = '&&, ||, ??';
+
+  //Operador de Atribuição
+  /**OBS: em outras linguagens pode aparecer assim: =+, ou =-    ==== comentário bloco*/
+  localAtribuicao: string = ' = , +=, -=';
+
+  //Operador Ternario
+  //cursos
+  localTernario: string = " localName = this.cursos ? 'curso de Java' : 'curso de JS' ";
+
+  constructor() {
+    let exemploNumber = 10; // aqui tenho 10
+    ///quanto tem na variavel exemploNumber ???? 10+20=30
+    exemploNumber += 20; // aqui tenho 30
+
+    /**ternario permite fazer fazer primeiro uma checagem, exemplo abaixo, se variavel for igual a 30 faço a atribuição de 50 
+       se não fica 100 */
+
+    let localTernario = exemploNumber ? 50 : 100;
+    let localTernario2 = exemploNumber === 30 ? 50 : 100;
+  }
+}
--- aula9junho/src/app/pages/variaveis/variaveis.ts
+++ aula9junho/src/app/pages/variaveis/variaveis.ts
@@ -1,9 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-variaveis',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './variaveis.html',
   styleUrl: './variaveis.css',
 })
-export class Variaveis {}
+export class Variaveis {
+  ///Variaveis globais elas estão em todo o escopo
+  nome = 'Ana';
+  localObjecto = { nome: 'Tony', endereco: 'rua...', numero: 10, admin: false, lado: 'esquerdo' };
+
+  constructor() {
+    console.log('Minha variavel global Objecto no construtor: ', this.localObjecto);
+    /// Invocar no construtor
+    this.metodoTestConst();
+    this.metodoTestLet();
+    this.metodoTestConst2();
+    this.metodoTestLet2();
+  }
+
+  metodoTestLet() {
+    console.log('Minha variavel global Objecto no metodoTestLet: ', this.localObjecto);
+    this.nome = 'Rafael';
+    console.log('Minha variavel global nome metodoTestLet: ', this.localObjecto);
+  }
+  metodoTestConst() {
+    console.log('Minha variavel global Objecto no metodoTestConst: ', this.localObjecto);
+    this.nome = 'Paulo';
+    console.log('Minha variavel global nome no metodoTestConst: ', this.localObjecto);
+  }
+
+  metodoTestConst2 = () => {
+    //var nome = 'tony';
+    // escopo local NÃO usa this.
+    const localNome = 'tony filho';
+    console.log('Minha variavel const localNome no metodoTestConst(): ', localNome);
+    //localNome = "ana"   sendo constante não pode mudar
+  };
+
+    metodoTestLet2 = () => {
+    //var nome = 'tony';
+    // escopo local NÃO usa this.
+    let localNome = 'tony filho';
+    console.log('Minha variavel const localNome no metodoTestConst(): ', localNome);
+    localNome ="Monica"
+    console.log('Minha variavel const localNome no metodoTestConst(): ', localNome);
+  };
+}
