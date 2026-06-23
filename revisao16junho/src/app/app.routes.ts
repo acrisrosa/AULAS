895278736dd84407b7f318b72c36dcaf7ef57dec
acrisrosa <cris2022pereira@gmail.com>
2026-06-23 22:56:12 +0100

    criação rotas entre páginas

--- revisao16junho/src/app/app.routes.ts
+++ revisao16junho/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+import { Lacos } from './pages/lacos/lacos';
 
 export const routes: Routes = [
-    {path: " ", redirectTo: "ebook/welcome", pathMatch: 'full'},   // " " ou ' '
-   
-    /** ultima rota */
-    {path: '**',loadComponent: () => import('./pages/welcome/welcome').then(c => c.Welcome)},
+  { path: ' ', redirectTo: 'ebook/welcome', pathMatch: 'full' }, // " " ou ' '
+  { path: "ebook/welcome", loadComponent: () => import("./pages/welcome/welcome").then(ana => ana.Welcome)},
+  { path: "ebook/lacos", component: Lacos},
+  { path: 'ebook/tipo-de-dados', loadComponent: () => import("./pages/tipo-de-dados/tipo-de-dados").then(paulo => paulo.TipoDeDados)},
+  { path: "ebook/variaveis", loadComponent: () => import("./pages/variaveis/variaveis").then(leo => leo.Variaveis)},
+  { path: "ebook/operadores", loadComponent: () => import("./pages/operadores/operadores").then(c => c.Operadores)},
+  { path: 'ebook/estrutura-de-controles', loadComponent: () => import('./pages/estrutura-de-controles/estrutura-de-controles').then(c => c.EstruturaDeControles)},
+  /** ultima rota */
+  { path: '**', loadComponent: () => import('./pages/welcome/welcome').then((c) => c.Welcome)},
 ];
