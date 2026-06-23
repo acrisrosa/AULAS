8e00298b2b8898da835608139f27846c8f1c65a9
acrisrosa <cris2022pereira@gmail.com>
2026-06-23 22:23:00 +0100

    criação de páginas

--- revisao16junho/src/app/app.routes.ts
+++ revisao16junho/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: " ", redirectTo: "ebook/welcome", pathMatch: 'full'},   // " " ou ' '
+   
+    /** ultima rota */
+    {path: '**',loadComponent: () => import('./pages/welcome/welcome').then(c => c.Welcome)},
+];
