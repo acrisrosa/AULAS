73406077a9bf7d3eba3812cd9e9cdfa6768cf895
acrisrosa <cris2022pereira@gmail.com>
2026-06-24 19:49:55 +0100

    inicio da aula 16junho

--- aula16junho/src/app/app.routes.ts
+++ aula16junho/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'ebook/interpolacao', pathMatch: 'full' },
+  {
+    path: 'ebook/interpolacao',
+    loadComponent: () =>
+      import('./pages/interpolacao/interpolacao').then((ana) => ana.Interpolacao),
+  },
+  {
+    path: 'ebook/interface',
+    loadComponent: () => import('./pages/interfaces/interfaces').then((rafael) => rafael.Interfaces),
+  },
+    {
+    path: 'ebook/poo',
+    loadComponent: () => import('./pages/poo/poo').then((leo) => leo.Poo),
+  },
+    {
+    path: 'ebook/functions',
+    loadComponent: () => import('./pages/functions/functions').then((c) => c.Functions),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/interpolacao/interpolacao').then((c) => c.Interpolacao),
+  },
+];
